cf45451138d637e16b651dc73471262f5a540517
ahadout <mohamedahdoutt@gmail.com>
2022-06-10 10:21:03 +0100

    save

--- pages/basic_infos.php
+++ pages/basic_infos.php
@@ -20,7 +20,44 @@
             </div>
             <p class="p2">Describe your service</p>
         </div>
+        <div class="container_3">
+            <div class="_3">
+            <div class="_line3"></div>
+                <div>
+                    <span>3</span>
+                </div>
+                <div class="line3_"></div>
+            </div>
+            <p class="p3">Describe your service</p>
+        </div>
     </div>
+    <section class="add_post_section">
+        <div class="add_post_form">
+            <h2>general informations</h2>
+            <form>
+                <div>
+                    <label for="type">Type of service:</label>
+                    <select id="service_type" name="type">
+                        <option value="maison">Maison</option>
+                        <option value="mini_hotel">Mini hotel</option>
+                        <option value="thing_to_do">Thing to do</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="city">City:</label>
+                    <select id="city" name="city">
+                        <option value="tangier">Tangier</option>
+                        <option value="tetouan">Tetouane</option>
+                        <option value="houceima">Houceima</option>
+                    </select>
+                </div>
+                <div class="button">
+                    <button type="submit">Next</button>
+                </div>
+                
+            </form>
+        </div>
+    </section>
 </main>
 <style><?php include('../css/style.css') ?></style>
 <?php include('footer.php') ?>
